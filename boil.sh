5be73a70531ea2a2bee7d4bebc6d738f21363e03
Viktor von Drakk <viktorvondrakk@gmail.com>
2017-05-24 16:49:30 -0700

    added boil.sh

---
+++ boil.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+# Pass new project name as argument
+# todo: would be nice to have github create a non-existant repo
+perl -pi -e 's/ReactStub/$1/' .git/config
+npm install
+git init
+git add .
+git commit -m "initial check-in"
+git push
+# heroku init tasks
+heroku create
+git push heroku
